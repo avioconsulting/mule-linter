eb1db432b05ab62466a5ee9b5f525ed20491b222
Nisarg Pansuria <npansuria@avioconsulting.com>
2020-07-09 11:50:37 -0500

    Refactor for code review.

--- src/main/groovy/com/avioconsulting/mule/linter/rule/configuration/FlowSubflowNamingRule.groovy
+++ src/main/groovy/com/avioconsulting/mule/linter/rule/configuration/FlowSubflowNamingRule.groovy
@@ -1,6 +1,7 @@
 package com.avioconsulting.mule.linter.rule.configuration
 
 import com.avioconsulting.mule.linter.model.Application
+import com.avioconsulting.mule.linter.model.CaseNaming
 import com.avioconsulting.mule.linter.model.ConfigurationFile
 import com.avioconsulting.mule.linter.model.MuleComponent
 import com.avioconsulting.mule.linter.model.Rule
@@ -8,30 +9,17 @@ import com.avioconsulting.mule.linter.model.RuleViolation
 
 class FlowSubflowNamingRule extends Rule {
 
-    enum CaseFormat {
-
-        CAMEL_CASE,
-        PASCAL_CASE,
-        SNAKE_CASE,
-        KEBAB_CASE
-
-    }
-
     static final String RULE_ID = 'FLOW_SUBFLOW_NAMING'
     static final String RULE_NAME = 'Flow and subflow name is following naming conventions'
     static final String RULE_VIOLATION_MESSAGE = 'Flow or subflow is not following naming conventions: '
     static final Map<String, String> flowSubFlowComponent = ['sub-flow':ConfigurationFile.MULE_CORE_NAMESPACE,
                                                             'flow':ConfigurationFile.MULE_CORE_NAMESPACE]
-    static final Map<CaseFormat,String> regex = [(CaseFormat.CAMEL_CASE):'[a-z]+((\\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?',
-                                                (CaseFormat.PASCAL_CASE):'^[A-Z][a-zA-Z0-9]+$',
-                                                (CaseFormat.SNAKE_CASE):'^([a-z][a-z0-9]*)(_[a-z0-9]+)*$',
-                                                (CaseFormat.KEBAB_CASE):'^([a-z][a-z0-9]*)(-[a-z0-9]+)*$']
-    CaseFormat format
+    CaseNaming caseNaming = new CaseNaming()
 
-    FlowSubflowNamingRule(CaseFormat format) {
+    FlowSubflowNamingRule(CaseNaming.CaseFormat format) {
         this.ruleId = RULE_ID
         this.ruleName = RULE_NAME
-        this.format = format
+        caseNaming.setFormat(format)
     }
 
     @Override
@@ -46,7 +34,7 @@ class FlowSubflowNamingRule extends Rule {
                 flowsSubflows.each {
                     flowSubflow ->
                     String flowSubflowName = flowSubflow.getAttributeValue('name')
-                    if (!isValidFormat(flowSubflowName)) {
+                    if (!caseNaming.isValidFormat(flowSubflowName)) {
                         violations.add(new RuleViolation(this, configFile.path,
                                 flowSubflow.getLineNumber(), RULE_VIOLATION_MESSAGE + flowSubflowName))
                     }
@@ -54,9 +42,5 @@ class FlowSubflowNamingRule extends Rule {
             }
         }
         return violations
-    }
-
-    Boolean isValidFormat(String value) {
-        return value.matches(regex.get(format))
     }
 }
--- src/test/groovy/com/avioconsulting/mule/linter/rule/configuration/FlowSubflowNamingRuleTest.groovy
+++ src/test/groovy/com/avioconsulting/mule/linter/rule/configuration/FlowSubflowNamingRuleTest.groovy
@@ -1,6 +1,7 @@
 package com.avioconsulting.mule.linter.rule.configuration
 
 import com.avioconsulting.mule.linter.model.Application
+import com.avioconsulting.mule.linter.model.CaseNaming
 import com.avioconsulting.mule.linter.model.Rule
 import com.avioconsulting.mule.linter.model.RuleViolation
 import spock.lang.Specification
@@ -10,18 +11,14 @@ class FlowSubflowNamingRuleTest extends Specification {
     private static final String NAMING_APP = 'FlowSubflowNaming'
 
     private static final String CAMEL_CASE_FILE = 'camelCase.xml'
-    private static final String CAMEL_CASE_INCORRECT_FILE = 'camelCase-incorrect.xml'
     private static final String PASCAL_CASE_FILE = 'PascalCase.xml'
-    private static final String PASCAL_CASE_INCORRECT_FILE = 'PascalCase-incorrect.xml'
     private static final String SNAKE_CASE_FILE = 'snake_case.xml'
-    private static final String SNAKE_CASE_INCORRECT_FILE = 'snake_case-incorrect.xml'
     private static final String KEBAB_CASE_FILE = 'kebab-case.xml'
-    private static final String KEBAB_CASE_INCORRECT_FILE = 'kebab-case-incorrect.xml'
 
     @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
     def 'cameCase flow subflow naming convention check'() {
         given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.CAMEL_CASE)
+        Rule rule = new FlowSubflowNamingRule(CaseNaming.CaseFormat.CAMEL_CASE)
 
         when:
         File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
@@ -30,36 +27,12 @@ class FlowSubflowNamingRuleTest extends Specification {
 
         then:
         !violations.any { it.fileName.contains(CAMEL_CASE_FILE) }
-
-        violations.any { it.fileName.contains(PASCAL_CASE_FILE) }
-        violations.count { it.fileName.contains(PASCAL_CASE_FILE) } == 4
-
-        violations.any { it.fileName.contains(SNAKE_CASE_FILE) }
-        violations.count { it.fileName.contains(SNAKE_CASE_FILE) } == 2
-
-        violations.any { it.fileName.contains(KEBAB_CASE_FILE) }
-        violations.count { it.fileName.contains(KEBAB_CASE_FILE) } == 2
-    }
-
-    @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
-    def 'Incorrect cameCase flow subflow naming convention check'() {
-        given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.CAMEL_CASE)
-
-        when:
-        File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
-        Application app = new Application(appDir)
-        List<RuleViolation> violations = rule.execute(app)
-
-        then:
-        violations.any { it.fileName.contains(CAMEL_CASE_INCORRECT_FILE) }
-        violations.count { it.fileName.contains(CAMEL_CASE_INCORRECT_FILE) } == 2
     }
 
     @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
     def 'PascalCase flow subflow naming convention check'() {
         given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.PASCAL_CASE)
+        Rule rule = new FlowSubflowNamingRule(CaseNaming.CaseFormat.PASCAL_CASE)
 
         when:
         File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
@@ -67,37 +40,13 @@ class FlowSubflowNamingRuleTest extends Specification {
         List<RuleViolation> violations = rule.execute(app)
 
         then:
-        violations.any { it.fileName.contains(CAMEL_CASE_FILE) }
-        violations.count { it.fileName.contains(CAMEL_CASE_FILE) } == 2
-
         !violations.any { it.fileName.contains(PASCAL_CASE_FILE) }
-
-        violations.any { it.fileName.contains(SNAKE_CASE_FILE) }
-        violations.count { it.fileName.contains(SNAKE_CASE_FILE) } == 2
-
-        violations.any { it.fileName.contains(KEBAB_CASE_FILE) }
-        violations.count { it.fileName.contains(KEBAB_CASE_FILE) } == 2
-    }
-
-    @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
-    def 'Incorrect PascalCase flow subflow naming convention check'() {
-        given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.PASCAL_CASE)
-
-        when:
-        File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
-        Application app = new Application(appDir)
-        List<RuleViolation> violations = rule.execute(app)
-
-        then:
-        violations.any { it.fileName.contains(PASCAL_CASE_INCORRECT_FILE) }
-        violations.count { it.fileName.contains(PASCAL_CASE_INCORRECT_FILE) } == 2
     }
 
     @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
     def 'snake_case flow subflow naming convention check'() {
         given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.SNAKE_CASE)
+        Rule rule = new FlowSubflowNamingRule(CaseNaming.CaseFormat.SNAKE_CASE)
 
         when:
         File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
@@ -105,37 +54,13 @@ class FlowSubflowNamingRuleTest extends Specification {
         List<RuleViolation> violations = rule.execute(app)
 
         then:
-        violations.any { it.fileName.contains(CAMEL_CASE_FILE) }
-        violations.count { it.fileName.contains(CAMEL_CASE_FILE) } == 2
-
-        violations.any { it.fileName.contains(PASCAL_CASE_FILE) }
-        violations.count { it.fileName.contains(PASCAL_CASE_FILE) } == 4
-
         !violations.any { it.fileName.contains(SNAKE_CASE_FILE) }
-
-        violations.any { it.fileName.contains(KEBAB_CASE_FILE) }
-        violations.count { it.fileName.contains(KEBAB_CASE_FILE) } == 2
-    }
-
-    @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
-    def 'Incorrect snake_case flow subflow naming convention check'() {
-        given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.SNAKE_CASE)
-
-        when:
-        File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
-        Application app = new Application(appDir)
-        List<RuleViolation> violations = rule.execute(app)
-
-        then:
-        violations.any { it.fileName.contains(SNAKE_CASE_INCORRECT_FILE) }
-        violations.count { it.fileName.contains(SNAKE_CASE_INCORRECT_FILE) } == 2
     }
 
     @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
     def 'kebab-case flow subflow naming convention check'() {
         given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.KEBAB_CASE)
+        Rule rule = new FlowSubflowNamingRule(CaseNaming.CaseFormat.KEBAB_CASE)
 
         when:
         File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
@@ -143,30 +68,6 @@ class FlowSubflowNamingRuleTest extends Specification {
         List<RuleViolation> violations = rule.execute(app)
 
         then:
-        violations.any { it.fileName.contains(CAMEL_CASE_FILE) }
-        violations.count { it.fileName.contains(CAMEL_CASE_FILE) } == 2
-
-        violations.any { it.fileName.contains(PASCAL_CASE_FILE) }
-        violations.count { it.fileName.contains(PASCAL_CASE_FILE) } == 4
-
-        violations.any { it.fileName.contains(SNAKE_CASE_FILE) }
-        violations.count { it.fileName.contains(SNAKE_CASE_FILE) } == 2
-
         !violations.any { it.fileName.contains(KEBAB_CASE_FILE) }
-    }
-
-    @SuppressWarnings(['MethodName', 'MethodReturnTypeRequired'])
-    def 'Incorrect kebab-case flow subflow naming convention check'() {
-        given:
-        Rule rule = new FlowSubflowNamingRule(FlowSubflowNamingRule.CaseFormat.KEBAB_CASE)
-
-        when:
-        File appDir = new File(this.class.classLoader.getResource(NAMING_APP).file)
-        Application app = new Application(appDir)
-        List<RuleViolation> violations = rule.execute(app)
-
-        then:
-        violations.any { it.fileName.contains(KEBAB_CASE_INCORRECT_FILE) }
-        violations.count { it.fileName.contains(KEBAB_CASE_INCORRECT_FILE) } == 2
     }
 }
