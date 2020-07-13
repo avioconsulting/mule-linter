ea14c78f6f899f9462d7d5e52252c5284ab4bdde
Kevin King <kking@avioconsulting.com>
2020-07-13 13:01:27 -0400

    MIP-424 Created Rule and Test

---
+++ src/main/groovy/com/avioconsulting/mule/linter/rule/configuration/OnErrorLogExceptionRule.groovy
@@ -0,0 +1,37 @@
+package com.avioconsulting.mule.linter.rule.configuration
+
+import com.avioconsulting.mule.linter.model.Application
+import com.avioconsulting.mule.linter.model.Rule
+import com.avioconsulting.mule.linter.model.RuleViolation
+
+class OnErrorLogExceptionRule extends Rule {
+
+    static final String RULE_ID = 'ON_ERROR_LOG_EXCEPTION'
+    static final String RULE_NAME = 'On Error Enable Log Exception Rule'
+    static final String RULE_VIOLATION_MESSAGE = 'Log exception enabled is required for ' +
+                                                    'on-error-continue and on-error-propagate'
+    private static final List<String> ON_ERROR_COMPONENTS = ['on-error-continue', 'on-error-propagate']
+    private static final String ATTRIBUTE_NAME = 'logException'
+    private static final String ATTRIBUTE_VALUE_CHECK = 'false'
+
+    OnErrorLogExceptionRule() {
+        this.ruleId = RULE_ID
+        this.ruleName = RULE_NAME
+    }
+
+    @Override
+    List<RuleViolation> execute(Application application) {
+        List<RuleViolation> violations = []
+        application.configurationFiles.each { file ->
+            ON_ERROR_COMPONENTS.each {
+                file.findComponents(it, file.MULE_CORE_NAMESPACE).each { comp ->
+                    if (comp.getAttributeValue(ATTRIBUTE_NAME).equalsIgnoreCase(ATTRIBUTE_VALUE_CHECK)) {
+                        violations.add(new RuleViolation(this, file.name, comp.lineNumber, RULE_VIOLATION_MESSAGE))
+                    }
+                }
+            }
+        }
+        return violations
+    }
+
+}
