8814dc6eaebb5db1a6e4472b9c268a9d13f1da6a
Nisarg Pansuria <npansuria@avioconsulting.com>
2020-07-17 16:18:58 -0500

    MunitPluginVersionRule RULE_ID rename.

--- src/main/groovy/com/avioconsulting/mule/linter/rule/pom/MunitPluginVersionRule.groovy
+++ src/main/groovy/com/avioconsulting/mule/linter/rule/pom/MunitPluginVersionRule.groovy
@@ -5,7 +5,7 @@ import com.avioconsulting.mule.linter.model.rule.RuleViolation
 
 class MunitPluginVersionRule extends PomPluginAttributeRule {
 
-    static final String RULE_ID = 'MUNIT_MAVEN_PLUGIN'
+    static final String RULE_ID = 'MUNIT_PLUGIN_VERSION'
     static final String RULE_NAME = 'Munit maven plugin version'
     static final String GROUP_ID = 'com.mulesoft.munit.tools'
     static final String ARTIFACT_ID = 'munit-maven-plugin'
