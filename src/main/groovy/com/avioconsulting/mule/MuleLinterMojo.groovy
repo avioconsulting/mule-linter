a3be3a78694d88a334c27ea7a9321131ca537636
Reed McCartney <rfm.mccartney@gmail.com>
2020-10-06 10:57:49 -0500

    Get maven-plugin plugin to run

--- src/main/groovy/com/avioconsulting/mule/MuleLinterMojo.groovy
+++ src/main/groovy/com/avioconsulting/mule/MuleLinterMojo.groovy
@@ -6,20 +6,17 @@ import org.apache.maven.plugin.MojoFailureException
 import org.apache.maven.plugins.annotations.LifecyclePhase
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
-import org.apache.maven.project.MavenProject
 
 @Mojo(name = 'MuleLinter', defaultPhase = LifecyclePhase.COMPILE)
 class MuleLinterMojo extends AbstractMojo {
-    @Parameter(defaultValue = "${project}", required = true, readonly = true)
-    MavenProject project
+    @Parameter(defaultValue = '${project.basedir.path}', required = true, readonly = true)
+    String appDir
 
-    @Parameter(property = "muleLinter.config", defaultValue = "RuleConfiguration.groovy")
+    @Parameter(property = 'muleLinter.config', defaultValue = 'RuleConfiguration.groovy')
     String ruleConfiguration
 
     @Override
     void execute() throws MojoExecutionException, MojoFailureException {
-        String appDir = project.basedir
-
         MuleLinter ml = new MuleLinter(appDir, ruleConfiguration)
         ml.runLinter()
     }
