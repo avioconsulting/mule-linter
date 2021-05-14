b218ac9698faada18cdc3eaa45e6a6233b468e7b
Andres <iaivaldi@gmail.com>
2021-05-14 11:37:28 -0300

    fix: change BSON strategy to create json

--- src/main/groovy/com/avioconsulting/mule/MuleLinter.groovy
+++ src/main/groovy/com/avioconsulting/mule/MuleLinter.groovy
@@ -11,9 +11,10 @@ class MuleLinter {
     List<RuleSet> ruleSetList = []
     String  outputFormat
 
-    MuleLinter(String applicationDirectory, String ruleConfigFile) {
+    MuleLinter(String applicationDirectory, String ruleConfigFile, String outputFormat) {
         this.app = new Application(new File(applicationDirectory))
         ruleSetList = parseConfigurationFile(ruleConfigFile)
+        this.outputFormat= outputFormat
     }
 
     List<RuleSet> parseConfigurationFile(String ruleConfigFile) {
