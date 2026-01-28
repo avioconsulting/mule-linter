72dcfe81a1882b6b964acb8e81a7111b6ab0ff7a
cflesher-c-reyes <cflesher@avioconsulting.com>
2026-01-28 10:17:36 -0600

    fix: handle empty YAML property files

---
+++ mule-linter-spi/src/test/groovy/com/avioconsulting/mule/linter/model/PropertyFileEmptyYamlTest.groovy
@@ -0,0 +1,59 @@
+package com.avioconsulting.mule.linter.model
+
+import spock.lang.Specification
+
+class PropertyFileEmptyYamlTest extends Specification {
+
+    private File appDir
+    private File yamlFile
+
+    def setup() {
+        appDir = File.createTempDir()
+        yamlFile = new File(appDir, 'empty.yaml')
+    }
+
+    def cleanup() {
+        yamlFile.delete()
+        appDir.deleteDir()
+    }
+
+    def "empty yaml file does not throw"() {
+        given:
+        yamlFile.text = ''
+
+        when:
+        PropertyFile pf = new PropertyFile(yamlFile)
+
+        then:
+        noExceptionThrown()
+        pf.getProperties() != null
+        pf.getProperties().size() == 0
+    }
+
+    def "whitespace-only yaml file does not throw"() {
+        given:
+        // Tabs are not valid YAML indentation and will throw a SnakeYAML ScannerException.
+        yamlFile.text = '\n  \n  \n'
+
+        when:
+        PropertyFile pf = new PropertyFile(yamlFile)
+
+        then:
+        noExceptionThrown()
+        pf.getProperties() != null
+        pf.getProperties().size() == 0
+    }
+
+    def "comment-only yaml file does not throw"() {
+        given:
+        yamlFile.text = '# placeholder\n'
+
+        when:
+        PropertyFile pf = new PropertyFile(yamlFile)
+
+        then:
+        noExceptionThrown()
+        pf.getProperties() != null
+        pf.getProperties().size() == 0
+    }
+}
